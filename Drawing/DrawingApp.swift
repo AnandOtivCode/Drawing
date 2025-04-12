a92b092b6181ab74c008fc659e509c092f641e5c
Anand Otiv <w0858857@myscc.ca>
2025-04-12 19:31:33 -0400

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Anand Otiv on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
