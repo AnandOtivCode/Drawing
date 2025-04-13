7b94bfac689f36a7ffd087305796b28529dfe156
Anand Otiv <w0858857@myscc.ca>
2025-04-12 20:03:31 -0400

    Completed Paths vs Shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,22 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.closeSubpath()
+    struct Triangle: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
 
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+            return path
         }
-        //.fill(.blue)
-        //.stroke(.blue, lineWidth: 10)
-        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+    
+    struct Arc: Shape {
+        var startAngle: Angle
+        var endAngle: Angle
+        var clockwise: Bool
+        
+        
+
+        func path(in rect: CGRect) -> Path {
+            let rotationAdjustment = Angle.degrees(90)
+                let modifiedStart = startAngle - rotationAdjustment
+                let modifiedEnd = endAngle - rotationAdjustment
+            var path = Path()
+//            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+            path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+            return path
+        }
+    }
+
+    var body: some View {
+//        Triangle()
+////            .fill(.red)
+////            .frame(width: 300, height: 300)
+//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//                .frame(width: 300, height: 300)
+        
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+        
+
 
 
 
     }
+    
+    
+    
+//    var body: some View {
+//        Path { path in
+//            path.move(to: CGPoint(x: 200, y: 100))
+//            path.addLine(to: CGPoint(x: 100, y: 300))
+//            path.addLine(to: CGPoint(x: 300, y: 300))
+//            path.addLine(to: CGPoint(x: 200, y: 100))
+//            path.closeSubpath()
+//
+//        }
+//        //.fill(.blue)
+//        //.stroke(.blue, lineWidth: 10)
+//        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//
+//
+//
+//    }
 }
 
 #Preview {
